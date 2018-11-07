3fbdc3b97b52abbd4d6c7c063ba6a2fd145ee5c0
Administrator <gk0003@naver.com>
2018-11-07 12:38:59 +0900

    모든 필드를 삭제

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,3 @@
-public class Test { //git에서 관리를 안하기 때문에 빨간색 초록색은 번호를 부여 받았다.
-    private int f1;
-    private int f2;
-    private int f3;
-    private int f4;
-    private int f5;
-    private int f6;
-    private int f7;
-    private int f8;
-    private int f9;
-    private int f10;
-    private int f11;
-    private int f12;
+public class Test { //u1 git에서 관리를 안하기 때문에 빨간색 초록색은 번호를 부여 받았다.
+
 }
